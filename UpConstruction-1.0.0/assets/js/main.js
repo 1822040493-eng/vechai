9670c2b47c07386b33767d9e8d31bf98dda70679
ProudNguyen <nguyenhuuhao9@gmail.com>
2026-03-19 16:51:37 +0700

    fix index.html

--- UpConstruction-1.0.0/assets/js/main.js
+++ UpConstruction-1.0.0/assets/js/main.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const mobileNavHide = document.querySelector('.mobile-nav-hide');
 
   document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
-    el.addEventListener('click', function(event) {
+    el.addEventListener('click', function (event) {
       event.preventDefault();
       mobileNavToogle();
     })
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
 
   navDropdowns.forEach(el => {
-    el.addEventListener('click', function(event) {
+    el.addEventListener('click', function (event) {
       if (document.querySelector('.mobile-nav-active')) {
         event.preventDefault();
         this.classList.toggle('active');
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
    */
   const scrollTop = document.querySelector('.scroll-top');
   if (scrollTop) {
-    const togglescrollTop = function() {
+    const togglescrollTop = function () {
       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
     }
     window.addEventListener('load', togglescrollTop);
@@ -117,8 +117,8 @@ document.addEventListener('DOMContentLoaded', () => {
       });
 
       let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
-      menuFilters.forEach(function(el) {
-        el.addEventListener('click', function() {
+      menuFilters.forEach(function (el) {
+        el.addEventListener('click', function () {
           document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
           this.classList.add('filter-active');
           portfolioIsotope.arrange({
@@ -185,6 +185,48 @@ document.addEventListener('DOMContentLoaded', () => {
       1200: {
         slidesPerView: 2,
         spaceBetween: 20
+      }
+    }
+  });
+
+  /**
+   * Init materials slider
+   */
+  new Swiper('.slides-materials', {
+    speed: 850,
+    loop: true,
+    centeredSlides: false,
+    grabCursor: true,
+    autoplay: {
+      delay: 2400,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: false
+    },
+    effect: 'slide',
+    pagination: {
+      el: '.materials-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.materials-button-next',
+      prevEl: '.materials-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 4,
+        spaceBetween: 8,
+        centeredSlides: false
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        centeredSlides: false
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 12,
+        centeredSlides: false
       }
     }
   });
